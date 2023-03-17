d4912ba992e44153f2bfbd922d23e56df626f340
michael-ferreira <93886512+michaelferreira969@users.noreply.github.com>
2023-03-17 02:02:52 -0300

    Finalização do projeto

--- js/index.js
+++ js/index.js
@@ -15,6 +15,58 @@ class Pessoa {
   }
 }
 
+//Variáveis
+
+let listLojas = [];
+let loja = {};
+
+let listPessoas = [];
+let pessoa = {};
+
+/*Nova loja */
+loja = new Loja("Supremo Bolos", "Rua Florida, 1.410 Brooklin", 51022579)
+listLojas.push(loja);
+
+loja = new Loja("Sodie Doces", "R. Carlos Rega, 74 - Brooklin", 55053434)
+listLojas.push(loja);
+
+loja = new Loja("Isabela Akkari", "R. Comendador Miguel Calfat, 410 - Vila Nova Conceição", 50961915)
+listLojas.push(loja);
+
+loja = new Loja("Fábrica de Bolo Vó Alzira", "Av. Padre Antônio José dos Santos, 1690 - Cidade Monções", 975635523)
+listLojas.push(loja);
+
+/*Nova pessoa*/
+pessoa = new Pessoa("Michael", "michaelferrys969@gmail.com", 953695641)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Douglas", "douglas@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Emerson", "emerson@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Marcelo", "marcelo@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Gabriel", "gabriel@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Rafael", "rafael@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Luís", "luis@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Vicenzo", "vicenzo@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Matheu", "vicenzo@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
+pessoa = new Pessoa("Liliane", "vicenzo@gmail.com", 12345678)
+listPessoas.push(pessoa);
+
 //Eventos
 
 /*Ocultar div ao carregar página*/
@@ -22,6 +74,9 @@ $("#divSorteado").hide();
 
 /*Ao clicar no botão sortear*/
 $(document).on("click", "#btnSortear", function () {
+  /*Pegar a pessoa escolhida*/
+  let pessoaSelected =  $("#listarPessoas").val();
+
   /*ocultar div*/
   $("#divSorteio").hide();
 
@@ -29,14 +84,13 @@ $(document).on("click", "#btnSortear", function () {
   $("#divSorteado").show();
 
   //Chama função que faz sorteio
-  let numSorteado = Sorteio(); 
+  let numSorteado = sorteio(); 
 
-  PreencherDivSorteado(numSorteado);
+  preencherDivSorteado(numSorteado, pessoaSelected);
 });
 
 /*Ao clicar no botão voltar*/
 $(document).on("click", "#btnVoltar", function () {
-  console.log()
   /*Limpar campos*/
   $("#nomePessoa").empty();
   $("#nomeLoja").empty();
@@ -50,50 +104,109 @@ $(document).on("click", "#btnVoltar", function () {
   $("#divSorteado").hide();
 });
 
-let listLojas = [];
-let loja = {};
+/*Adicionar nova loja*/
+$(document).on("click", "#btnAddLoja", function () {
+  //Pegar campos preenchidos
+  let data = [
+    nome = $("#iNomeLoja").val(),
+    endereco = $("#iEnderecoLoja").val(),
+    contato = parseInt($("#iContatoLoja").val())
+  ];
 
-let listPessoas = [];
-let pessoa = {};
+  addLoja(data);
 
-/*Nova loja */
-loja = new Loja("Supremo", "Berrine, 7984", 12114789)
-listLojas.push(loja);
+  //Limpar campos
+  $("#iNomeLoja").val("");
+  $("#iEnderecoLoja").val("");
+  $("#iContatoLoja").val("");
 
-loja = new Loja("Teste", "Flórida, 7984", 7777777)
-listLojas.push(loja);
+  //Retorna mensagem de sucesso
+  $.toast({
+    heading: 'Success',
+    text: 'Loja Adicionada com sucesso!',
+    showHideTransition: 'slide',
+    icon: 'success'
+  });
 
-loja = new Loja("Sodie", "Flórida, 7984", 7777777)
-listLojas.push(loja);
+  //Fechar modal
+  $(".close-modal").click();  
+});
 
-loja = new Loja("Padaria", "Flórida, 7984", 7777777)
-listLojas.push(loja);
+/*Adicionar nova pessoa*/
+$(document).on("click", "#btnAddPessoa", function () {
+  //Pegar campos preenchidos
+  let data = [
+    nome = $("#iNomePessoa").val(),
+    email = $("#iEmailPessoa").val(),
+    contato = parseInt($("#iContatoPessoa").val())
+  ];
 
-loja = new Loja("Carrefour", "Flórida, 7984", 7777777)
-listLojas.push(loja);
+  addPessoa(data);
 
-/*Nova pessoa*/
-pessoa = new Pessoa("Michael", "michael@", 12114789)
-listPessoas.push(pessoa);
+  //Limpar campos
+  $("#iNomePessoa").val("");
+  $("#iEmailPessoa").val("");
+  $("#iContatoPessoa").val("");
 
-pessoa = new Pessoa("Douglas", "michael@", 12114789)
-listPessoas.push(pessoa);
+  //Retorna mensagem de sucesso
+  $.toast({
+    heading: 'Success',
+    text: 'Pessoa Adicionada com sucesso!',
+    showHideTransition: 'slide',
+    icon: 'success'
+  });
 
-pessoa = new Pessoa("Andy", "michael@", 12114789)
-listPessoas.push(pessoa);
+  //Fechar modal
+  $(".close-modal").click();  
+});
 
-pessoa = new Pessoa("J", "michael@", 12114789)
-listPessoas.push(pessoa);
+/*Montar dados da tabela Loja*/
+$(document).on("click", "#btnModalVerPessoas", function () {
+  $("#tableVerLojas tbody").empty();
+  preencherTableLojas(listLojas);
+});
+
+/*Montar dados da tabela Pessoa*/
+$(document).on("click", "#btnModalVerPessoas", function () {
+  $("#tableVerPessoas tbody").empty();
+  preencherTablePessoa(listPessoas);
+});
 
 //Funções
 
-function Sorteio() {
+/*Adicionar a lista de opções*/
+loadOption();
+
+function loadOption() {
+  /*Adicionar opções ao dropList*/
+  listPessoas.forEach(pessoa => {
+    $("#listarPessoas").append($('<option>', {
+      value: pessoa.nome,
+      text: pessoa.nome
+    }));
+  });
+}
+
+function addPessoaOption(pessoa) {
+  /*Adicionar pessoa ao dropList*/
+  $("#listarPessoas").append($('<option>', {
+    value: pessoa.nome,
+    text: pessoa.nome
+  }));
+}
+
+function sorteio() {
   return Math.floor(Math.random() * (listLojas.length));
 }
 
-function PreencherDivSorteado(numSorteado) {
-  let pessoaSorteada = listPessoas[numSorteado];
+function preencherDivSorteado(numSorteado, pessoaSelected) {  
   let lojaSorteada = listLojas[numSorteado];
+  let pessoaSorteada = null;
+
+  if(pessoaSelected) //Busca a pessoa selecionada pelo nome
+    pessoaSorteada = listPessoas.find(p => p.nome == pessoaSelected); 
+  else //Sorteia pessoa aleatória
+    pessoaSorteada = listPessoas[numSorteado]; 
  
   //preenche o campo de nome da pessoa
   $("#nomePessoa").append(pessoaSorteada.nome);
@@ -106,70 +219,53 @@ function PreencherDivSorteado(numSorteado) {
   
   //preenche o campo de contato da loja
   $("#contatoLoja").append(lojaSorteada.contato);
-
-  AlterarImagemLoja(lojaSorteada.nome);
 }
 
-/*Mudar a imagem da loja*/
-function AlterarImagemLoja(nomeLoja) {
-
-  if(nomeLoja == "Sodie") 
-    $("#img-loja").attr("src", "img/bg-1.jpg");
-  else if(nomeLoja == "Supremo")
-    $("#img-loja").attr("src", "img/bg-2.jpg");
-  else 
-    $("#img-loja").attr("src", "img/bg-3.jpg");
+function addLoja(data) {
+  let newLoja = new Loja(data[0], data[1], data[2]);
+  listLojas.push(newLoja);
 }
 
-/*Listar pessoas
-$("#listarPessoas").append(
-  listPessoas.forEach(p => {
-    `<option value="${p.Nome}">${p.Nome}</option>`
-}));
+function addPessoa(data) {
+  let newPessoa = new Pessoa(data[0], data[1], data[2]);
+  listPessoas.push(newPessoa);
 
-function cadastrarLoja(nome, endereco, contato) {
-    let loja = {
-        Nome: nome,
-        Endereco: endereco,
-        Contato: contato,
-    };
-
-    listLojas.push(loja);
+  /*Adicionar no dropList */
+  addPessoaOption(newPessoa);
 }
 
+function preencherTableLojas(listLojas) {
+  let linha = "";
+  let loja;
 
-function cadastrarPessoa(nome, endereco, contato) {
-    let loja = {
-        Nome: nome,
-        Endereco: endereco,
-        Contato: contato,
-    };
+  for(let i = 0; i < listLojas.length; i++) {
+    loja = listLojas[i];
 
-    listLojas.push(loja);
-}
+    linha += '<tr>';
+    linha += '<td class="data">' + loja.nome + '</td>';
+    linha += '<td class="data"> Rua... </td>';
+    linha += '<td class="data">' + loja.contato + '</td>';
+    linha += '</tr>';
 
-function cadastrarPessoa() {
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const celular = document.getElementById("celular").value;
-  
-    const pessoa = new Pessoa(nome, email, celular);
-
-    console.log(pessoa.toString());
+    $("#tableVerLojas tbody").prepend(linha);
+    linha = "";
   }
-  
+}
 
-/*
-class Pessoa {
-    constructor(nome, email, celular) {
-      this.nome = nome;
-      this.email = email;
-      this.celular = celular;
-    }
-  
-    toString() {
-      return `Nome: ${this.nome}, Email: ${this.email}, Celular: ${this.celular}`;
-    }
+function preencherTablePessoa(listPessoas) {
+  let linha = "";
+  let pessoa;
+
+  for(let i = 0; i < listPessoas.length; i++) {
+    pessoa = listPessoas[i];
+
+    linha += '<tr>';
+    linha += '<td class="data">' + pessoa.nome + '</td>';
+    linha += '<td class="data">' + pessoa.email + '</td>';
+    linha += '<td class="data">' + pessoa.contato + '</td>';
+    linha += '</tr>';
+
+    $("#tableVerPessoas tbody").prepend(linha);
+    linha = "";
   }
-*/
-  
+}
